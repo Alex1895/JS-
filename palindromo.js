dc8e03c4ff07c33f3428a0de9a075d8307b95bca
Alejandro_Alaniz <miguelitojacobito@gmail.com>
2018-10-04 17:12:51 -0500

    file check if some string is palindromo or not

---
+++ palindromo.js
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Strings</h2>
+
+<p id="demo"></p>
+
+<script>
+var x = "Ana";  // String written inside quotes
+//.split() x = ["J","o","h","n","","D","o","e"];
+//.reverse() && .join()
+var w = x.split("");
+w = w.reverse()
+w = w.join("");
+
+function palindromo(cadena){
+var i, ind;
+var cad1 = [];
+for (var x = 0; x>=cad1.length; x++){
+	if (cad1.)
+}
+}
+//console.log(w);
+//console.log(z);
+document.querySelector("#demo").innerHTML = w;
+document.querySelector("#demo").innerHTML = z;
+//getElementById("demo").innerHTML = x[2];
+</script>
+
+</body>
+</html>
+
+
