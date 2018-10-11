c044227a4add9718fc3b9d8dede505e038e1736b
Alejandro_Alaniz <miguelitojacobito@gmail.com>
2018-10-11 11:38:07 -0500

    more codes of palindromos
    abi code run correctly but palindromo_copy01 doesnt work correctly

--- palindromo_copy01.js
+++ palindromo_copy01.js
@@ -7,39 +7,43 @@ function reverse_String()
 {
   var v_input = document.getElementById("input_palindromo").value;
   var cadena = v_input;
-  //cadena = cadena.trim().replace(/[^A-Z0-9]/ig,"").toLowerCase();
+  var cadena2 = cadena.split('').reverse().join('');
   var cadaux = "";
   var html = document.getElementById("result_palindromo");
   if (cadena.length > 0)
   {
 	if(onlyletters(v_input))
 	{
-		cadena = cadena.trim().replace(/s/ig,"").toLowerCase();	
-  for( var i = (cadena.length - 1); i >= 0; i--)
-   {
+		cadena = cadena.toLowerCase().replace(/[^A-Z]/ig,"");	
+  	for( var i = (cadena.length - 1); i >= 0; i--)
+   	{
     	cadaux += cadena[i];
-    }
-   arrowFunc_Alert_Strings(cadena, cadaux);
-   document.getElementById("result_palindromo").innerHTML=cadena + "es lo mismo" + cadaux;
-    if (cadaux === cadena)
-    {
-        alert("es palindromo: " + cadaux);
-        html.innerHTML=cadaux;
-    }else{
-        alert ("no es palindromo "+ cadena);
-        html.innerHTML=cadena;
-    }
+    	}
+   	arrowFunc_Alert_Strings(cadena, cadaux);
+	alert(validString(cadena, cadaux));
+   	html.innerHTML= cadena + "es lo mismo" + cadena2;
 	}
     }else{
 	alert ("text field en blanco");
     }
 }
 
+function validString(a,b)
+{
+	if (a === b)
+    	{
+	 return a + "es Palindromo! ";
+    	}else{
+	 return a + " no es palindromo";
+    }	
+}
 function onlyletters(inputtxt){
-  var regex = /[^a-zA-Z]s/ig;
-  if(inputtxt.match(regex)){
-    alert ("por favor ingresa solamente caracteres validos");
-    return false;
-  }return true;
+  var regex = /^\d {0,9}$/gm;
+  if(inputtxt.match(regex))
+  {
+	alert ("por favor ingresa solamente caracteres validos");
+	return false; 
+  } 	
+	return true;      
 }
 reverse_String();
